001c5881b2d51425c38351610b5e06902f1ac059
unknown <jjamal95@gmail.com>
2019-02-27 17:18:38 -0600

    addeed the CardBanner image, and CardContent text to CardContainer

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Card.css';
 import HeaderContainer from '../HeaderComponents/HeaderContainer';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 
 const CardContainer = () => {
     return <div className="card-wrapper">
       <HeaderContainer/>
-     
+      <CardBanner/>
+      <CardContent/>
     </div>
   
   };
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = () => {
+    return <div><h5>Getting started with React</h5><p className="content-wrapper4">React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+    <h3>reactjs.org</h3>   </div>    
+}
+
+
+
+export default CardContent;
